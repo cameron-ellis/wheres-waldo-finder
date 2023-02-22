76b4fa28634ff8df6068a9e55ed7e0b061a16d8c
Cameron Ellis <cameronellis46@gmail.com>
2023-02-22 16:59:09 -0600

    add initial python script

---
+++ where-waldo.py
@@ -0,0 +1,12 @@
+# first import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+import os
+
+# tensorflow
+import tensorflow as tf
+from tensorflow.python.keras.utils.vis_utils import plot_model
+
+#import time
+import time
